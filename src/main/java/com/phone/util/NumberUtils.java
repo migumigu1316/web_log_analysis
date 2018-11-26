972dd8158da6b59c9942eeadb6a290597f0030e0
yang <migumigu1316@163.com>
2018-11-26 21:44:17 +0800

    提交

--- src/main/java/com/phone/util/NumberUtils.java
+++ src/main/java/com/phone/util/NumberUtils.java
@@ -18,7 +18,7 @@ public class NumberUtils {
      * @param scale 四舍五入的位数
      * @return 格式化小数
      */
-    public static double formatDouble(double num, int scale) {
+    public static double formatDouble (double num, int scale) {
         BigDecimal bd = new BigDecimal(num);
         return bd.setScale(scale, BigDecimal.ROUND_HALF_UP).doubleValue();
     }
